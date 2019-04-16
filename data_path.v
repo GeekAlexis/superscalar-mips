1df4d2a7f163891aff3c9694095a65a9f73754a9
Alexis Yang <yyang01@ucsb.edu>
2019-04-16 00:47:57 -0700

    Update data_path.v

--- data_path.v
+++ data_path.v
@@ -1,11 +1,15 @@
+// top level module
 module data_path(input clk, reset,
                  output [31:0] pcF, instrD, outM, writedataM, readdataM, 
                  output memwriteM);
     
+    // fetch stage signals
     wire [31:0] pcplus4F, pc_next; //pcF;
     wire [31:0] instrF;
     wire stallF;
-    //wire [31:0] instrD;
+    
+    // decode stage signals
+    // wire [31:0] instrD;
     wire equalD;
     wire [4:0] rsD, rtD, rdD;
     wire memwriteD, regwriteD, memtoregD, regdstD, alusrcD, se_zeD, branchD, start_multD, mult_signD;
@@ -13,6 +17,8 @@ module data_path(input clk, reset,
     wire [1:0] out_selD, pcsrcD;
     wire [31:0] readdata1D, readdata2D, muxa_outD, muxb_outD, sh_immD, se_immD, ze_immD, ext_immD, sl2_outD, pcplus4D, pcbranchD, pcjumpD;
     wire stallD, forwardaD, forwardbD;
+    
+    // execute stage signals
     wire memwriteE, regwriteE, memtoregE, regdstE, alusrcE, start_multE, mult_signE;
     wire [1:0] out_selE;
     wire [3:0] alu_opE;
@@ -21,20 +27,24 @@ module data_path(input clk, reset,
     wire [31:0] srcaE, srcbE, aluoutE, readdata1E, readdata2E, writedataE, outE, ext_immE, sh_immE, loE, hiE;
     wire flushE;
     wire [1:0] forwardaE, forwardbE;
+	
+    // memory stage signals
     wire regwriteM, memtoregM; //memwriteM
     wire [4:0] writeregM;
     //wire [31:0] writedataM, readdataM, outM;
+    
+    // write-back stage signals
     wire regwriteW, memtoregW;
     wire [4:0] writeregW;
     wire [31:0] outW, readdataW, resultW;
     
-    // fetch stage
+    // fetch stage logic
     mux3#(32) pc_mux(pcplus4F, pcbranchD, pcjumpD, pcsrcD, pc_next);
     register#(32) pc_reg(clk, reset, stallF, pc_next, pcF);
     inst_memory imem(pcF, instrF);
     assign pcplus4F = pcF + 4;
     
-    // decode stage
+    // decode stage logic
     register#(64) pipeline_regD(clk, reset | pcsrcD[0] | pcsrcD[1], stallD, {instrF, pcplus4F}, {instrD, pcplus4D});
     controller control(instrD[31:26], instrD[5:0], equalD, memwriteD, regwriteD, memtoregD, regdstD, alusrcD, se_zeD, branchD, start_multD, mult_signD, alu_opD, out_selD, pcsrcD);
     reg_file rf(clk, reset, regwriteW, instrD[25:21], instrD[21:16], writeregW, resultW, readdata1D, readdata2D);
@@ -52,7 +62,7 @@ module data_path(input clk, reset,
     assign pcbranchD = sl2_outD + pcplus4D;
     assign pcjumpD = {pcplus4D[31:28], instrD[25:0], 2'b0};
     
-    // execute stage
+    // execute stage logic
     wire [155:0] dE, qE;
     assign dE = {memwriteD, regwriteD, memtoregD, regdstD, alusrcD, alu_opD, out_selD, start_multD, mult_signD, readdata1D, readdata2D, rsD, rtD, rdD, sh_immD, ext_immD};
     assign {memwriteE, regwriteE, memtoregE, regdstE, alusrcE, alu_opE, out_selE, start_multE, mult_signE, readdata1E, readdata2E, rsE, rtE, rdE, sh_immE, ext_immE} = qE;
@@ -65,14 +75,14 @@ module data_path(input clk, reset,
     multiplier mult(clk, start_multE, mult_signE, srcaE, srcbE, {hiE, loE});
     mux4#(32) out_muxE(aluoutE, sh_immE, loE, hiE, out_selE, outE);
     
-    // memory stage
+    // memory stage logic
     wire [71:0] dM, qM;
     assign dM = {memwriteE, regwriteE, memtoregE, outE, writedataE, writeregE};
     assign {memwriteM, regwriteM, memtoregM, outM, writedataM, writeregM} = qM;
     register#(72) pipeline_regM(clk, reset, 0, dM, qM);
     data_memory dmem(clk, memwriteM, outM, writedataM, readdataM);
     
-    // write-back stage
+    // write-back stage logic
     wire [70:0] dW, qW;
     assign dW = {regwriteM, memtoregM, readdataM, outM, writeregM};
     assign {regwriteW, memtoregW, readdataW, outW, writeregW} = qW;
@@ -84,6 +94,8 @@ module data_path(input clk, reset,
                        writeregE, writeregM, writeregW, stallF, stallD, forwardaD, forwardbD, flushE, forwardaE, forwardbE);
                 
 endmodule
+
+// basic building blocks
 
 module register#(parameter width = 1)
                 (input clk, reset, en,
