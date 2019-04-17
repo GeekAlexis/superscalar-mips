26e25c78ffffbb5c7e5400c53a8edbfeeb392be6
Ning Du <duningdudasa1998@gmail.com>
2019-04-16 20:16:13 -0700

    Update data_memory.v

--- data_memory.v
+++ data_memory.v
@@ -1,14 +1,15 @@
 module data_memory(input clk, write,
                    input [31:0] address,
                    input [31:0] write_data,
-                   output [31:0] read_data);
+                   output reg [31:0] read_data);
 
-    reg [31:0] ram [0:63];
-    always @(posedge clk) begin
-        if(write)
-            ram[address[31:2]] <= write_data;
-    end
-    assign read_data = ram[address[31:2]];
+  reg [31:0] ram [0:63]; //64 words memory
     
-endmodule
-                   
+  always @(posedge clk) begin
+    if(write)
+      ram[address[31:2]] <= write_data; //write data
+    else
+      read_data <= ram[address[31:2]]; //write data
+  end
+    
+endmodule 
