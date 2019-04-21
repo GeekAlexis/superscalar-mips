8274872a8b8259c5233e9396a696ab7514da5d77
Ning Du <duningdudasa1998@gmail.com>
2019-04-21 14:58:58 -0700

    Update hazard_detector_tb.v

--- hazard_detector_tb.v
+++ hazard_detector_tb.v
@@ -2,31 +2,25 @@
 
 module hazard_detector_tb;
   
-  //reg clk = 0, reset = 0;
-  reg branchD; 
+  reg branchD;
   reg memtoregE, regwriteE;
-  reg memtoregM, regwriteM;
+  reg memtoregM;
   reg regwriteW;
   reg start_multE, busy_multE;
   reg [4:0] rsD, rtD, rsE, rtE;
-  reg [4:0] writeregE, writeregM, writeregW;
+  reg [4:0] writeregE, writeregM;
   wire stallF, stallD;
-  wire forwardaD, forwardbD;
   wire flushE;
-  wire [1:0] forwardaE, forwardbE;
  
-  hazard_detector dut (//clk, reset,
-                       branchD, 
-                       memtoregE, regwriteE,
-                       memtoregM, regwriteM,
-                       regwriteW,
-                       start_multE, busy_multE,
-                       rsD, rtD, rsE, rtE,
-                       writeregE, writeregM, writeregW,
-                       stallF, stallD,
-                       forwardaD, forwardbD,
-                       flushE,
-                       forwardaE, forwardbE);
+  hazard_detector dut (branchD,
+		       memtoregE, regwriteE,
+		       memtoregM,
+		       regwriteW,
+		       start_multE, busy_multE,
+		       rsD, rtD, rsE, rtE,
+		       writeregE, writeregM,
+		       stallF, stallD,
+		       flushE);
   initial begin
     //mult stall
     start_multE = 0; busy_multE = 0; #10; //stallF = stallD = flushE = x
@@ -46,32 +40,8 @@ module hazard_detector_tb;
     branchD = 1; rsD = 5; rtD = 6; writeregE = 7; regwriteE = 1; writeregM = 6; memtoregM = 1; #10; //stallF = stallD = flushE = 1
     branchD = 1; rsD = 5; rtD = 6; writeregE = 7; regwriteE = 1; writeregM = 6; memtoregM = 0; #10; //stallF = stallD = flushE = x
     rsD = 5'bx; rtD = 5'bx; writeregE = 5'bx; regwriteE = 1'bx; writeregM = 5'bx; memtoregM = 1'bx;
-    //forwardA decode stage
-    rsD = 0; writeregM = 0; regwriteM = 1; #10; //forwardaD = 0
-    rsD = 5; writeregM = 2; regwriteM = 1; #10; //forwardaD = 0
-    rsD = 5; writeregM = 5; regwriteM = 1; #10; //forwardaD = 1
-    rsD = 5; writeregM = 5; regwriteM = 0; #10; //forwardaD = 0 
-    rsD = 5'bx; writeregM = 5'bx; regwriteM = 1'bx;
-    //forwardB decode stage
-    rtD = 0; writeregM = 0; regwriteM = 1; #10; //forwardbD = 0
-    rtD = 5; writeregM = 2; regwriteM = 1; #10; //forwardbD = 0
-    rtD = 5; writeregM = 5; regwriteM = 1; #10; //forwardbD = 1
-    rtD = 5; writeregM = 5; regwriteM = 0; #10; //forwardbD = 0
-    rtD = 5'bx; writeregM = 5'bx; regwriteM = 1'bx;
-    //forwardA execution stage
-    rsE = 0; writeregM = 5; regwriteM = 1; writeregW = 5; regwriteW = 1; #10; //forwardaE = 2'b00
-    rsE = 4; writeregM = 5; regwriteM = 1; writeregW = 6; regwriteW = 1; #10; //forwardaE = 2'b00
-    rsE = 5; writeregM = 5; regwriteM = 1; writeregW = 5; regwriteW = 1; #10; //forwardaE = 2'b10
-    rsE = 5; writeregM = 4; regwriteM = 1; writeregW = 5; regwriteW = 1; #10; //forwardaE = 2'b01
-    rsE = 5'bx; writeregM = 5'bx; regwriteM = 1'bx; writeregW = 5'bx; regwriteW = 1'bx;
-    //forwardB execution stage
-    rtE = 0; writeregM = 5; regwriteM = 1; writeregW = 5; regwriteW = 1; #10; //forwardbE = 2'b00
-    rtE = 4; writeregM = 5; regwriteM = 1; writeregW = 6; regwriteW = 1; #10; //forwardbE = 2'b00
-    rtE = 5; writeregM = 5; regwriteM = 1; writeregW = 5; regwriteW = 1; #10; //forwardbE = 2'b10
-    rtE = 5; writeregM = 4; regwriteM = 1; writeregW = 5; regwriteW = 1; #10; //forwardbE = 2'b01
-    rtE = 5'bx; writeregM = 5'bx; regwriteM = 1'bx; writeregW = 5'bx; regwriteW = 1'bx;
     $stop;
 
   end
 
-endmodule 
+endmodule
