94775dcee4e35905d5eb1c9885f220d7eb1a82c5
Ning Du <duningdudasa1998@gmail.com>
2019-06-02 19:02:45 -0700

    Update inst_memory.v

--- inst_memory.v
+++ inst_memory.v
@@ -1,11 +1,12 @@
-module inst_memory(input [31:0] address,
-                   output [31:0] read_data);
+module inst_memory(input [31:0] pcF1, pcF2,
+                   output [31:0] instrF1, instrF2);
 
   reg [31:0] ram [0:127]; //memory with 128 32-bits words 
 
   initial
-    $readmemh("dotProduct.mem", ram); //load machine code
+    $readmemh("program.mem", ram); //load machine code
   
-  assign read_data = ram[address[31:2]]; //read from memory
+  assign instrF1 = ram[pcF1[31:2]]; //read from memory
+  assign instrF2 = ram[pcF2[31:2]];
  
 endmodule 
