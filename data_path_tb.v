69c3d7784db753f3e633822248a5cfbfc5bbb35d
Ning Du <duningdudasa1998@gmail.com>
2019-06-02 18:56:49 -0700

    Update data_path_tb.v

--- data_path_tb.v
+++ data_path_tb.v
@@ -1,46 +1,19 @@
 `timescale 1ns / 1ps
 
 module data_path_tb;
+  
+  reg clk;
+  reg reset = 1;
 
-    reg clk;
-    reg reset;
-    wire [31:0] pcF, instrD, outM, writedataM;
-    wire memwriteM;
-    wire [31:0] readdataM;
+  data_path dut(clk, reset);
     
-    data_path dut(clk, reset, pcF, instrD, outM, writedataM, readdataM, memwriteM);
+  initial begin // initialize test
+    #10; reset = 0;
+  end
     
-    // initialize test
-    initial begin
-        reset = 1; 
-        #10;
-        reset = 0;
-    end
-    
-    // generate clock to sequence tests
-    always begin
-        clk = 0;
-        #5;
-        clk = 1;
-        #5;
-    end
-    
-    // dot product result 4985
-    // check results
-    always @(negedge clk) begin
-        if(memwriteM) begin
-            if(outM === 0 & writedataM === 4985) begin
-                $display("Simulation succeeded");
-                #5;
-                $stop;
-            end
-            else begin
-                $display("Simulation failed");
-                #5;
-                $stop;
-            end
-        end
-    end
+  always begin // generate clock to sequence tests
+    clk = 0; #5;
+    clk = 1; #5;
+  end
 
 endmodule
-
