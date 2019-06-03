6d41c2bf84ec487275babe86580a0cb0d1c2d793
Ning Du <duningdudasa1998@gmail.com>
2019-06-02 19:08:28 -0700

    Update reg_file.v

--- reg_file.v
+++ reg_file.v
@@ -1,8 +1,8 @@
-module reg_file(input clk, reset, write,
-                input [4:0] pr1, pr2,
-                input [4:0] wr,
-                input [31:0] wd,
-                output [31:0] rd1, rd2);
+module reg_file(input clk, reset, write1, write2,
+                input [4:0] addr1D1, addr2D1, addr1D2, addr2D2,
+                input [4:0] wr1, wr2,
+                input [31:0] wd1, wd2,
+                output [31:0] rf_out1D1, rf_out2D1, rf_out1D2, rf_out2D2);
             
   reg [31:0] rf [0:31]; //32 registers
   integer i;
@@ -12,11 +12,18 @@ module reg_file(input clk, reset, write,
       for(i = 0; i < 32; i = i + 1) //reset all 32 registers
         rf[i] <= 32'd0;
       end
-    else if(write)
-      rf[wr] <= wd; //write data to register
+    else begin
+      if((wr1 == wr2) & write1 & write2) rf[wr2] <= wd2;
+      else begin
+        if(write1) rf[wr1] <= wd1;
+        if(write2) rf[wr2] <= wd2;
+      end
+    end
   end
   
-  assign rd1 = (pr1 != 0) ? rf[pr1] : 32'd0; //read from register with address pr1, $zero reads 0
-  assign rd2 = (pr2 != 0) ? rf[pr2] : 32'd0; //read from register with address pr2, $zero reads 0
+  assign rf_out1D1 = (addr1D1 != 0) ? rf[addr1D1] : 32'd0; 
+  assign rf_out2D1 = (addr2D1 != 0) ? rf[addr2D1] : 32'd0; 
+  assign rf_out1D2 = (addr1D2 != 0) ? rf[addr1D2] : 32'd0; 
+  assign rf_out2D2 = (addr2D2 != 0) ? rf[addr2D2] : 32'd0; 
 
 endmodule 
