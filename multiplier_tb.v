9a1581e9de2c5305dc12dcf827327bd485e05a21
Ning Du <duningdudasa1998@gmail.com>
2019-06-02 19:04:30 -0700

    Update multiplier_tb.v

--- multiplier_tb.v
+++ multiplier_tb.v
@@ -1,18 +1,18 @@
 `timescale 1ns / 1ps
 
 module multiplier_tb;
-  reg [31:0] a = 0;
-  reg [31:0] b = 0;
-  reg clk = 0, start = 0;
-  reg is_signed = 0;
-  wire busy;
+  reg clk = 0, multE, is_signedE;
+  reg [31:0] a, b;
+  wire mult_stall;
   wire[63:0] s;
-  integer i = 0;
+  integer i;
   reg [63:0] k;
   
-  multiplier dut (clk, start, is_signed, a, b, busy, s);
+  multiplier dut (clk, multE, is_signedE,
+                  a, b, 
+                  mult_stall, s);
   
-  task display; //task to make the clock tick
+  task display;
     begin
       #1; clk = ~clk;
       #1; clk = ~clk;
@@ -20,58 +20,45 @@ module multiplier_tb;
   endtask
   
   initial begin 
-       
-    //87359729 * 23422 signed
-    a = 87359729; b = 23422; is_signed = 1;
     
-    start = 1; display; start = 0;
-    for (i = 1; i < 65; i = i + 1) display;
-
-    k = 87359729 * 23422; //directly multiply to check the result
+    //87359729 * 23422 signed
+    multE = 1; is_signedE = 1; a = 87359729; b = 23422; 
+  
+    for (i = 0; i < 65; i = i + 1) display;
+    k = 87359729 * 23422; 
     
     //-77 * 999 signed
-    a = -77; b = 999; is_signed = 1;
+    multE = 1; is_signedE = 1; a = -77; b = 999;
     
-    start = 1; display; start = 0;
-    for (i = 1; i < 65; i = i + 1) display;
-
-    k = -77 * 999; //directly multiply to check the result
+    for (i = 0; i < 65; i = i + 1) display;
+    k = -77 * 999; 
     
     //809843053 * -328932 signed
-    a = 809843053; b = -328932; is_signed = 1;
-    
-    start = 1; display; start = 0;
-    for (i = 1; i < 65; i = i + 1) display;
+    multE = 1; is_signedE = 1; a = 809843053; b = -328932;
 
-    k = 809843053 * -328932; //directly multiply to check the result
+    for (i = 0; i < 65; i = i + 1) display;
+    k = 809843053 * -328932; 
     
     //-2147483648 * -2147483648 signed
-    a = -2147483648; b = -2147483648; is_signed = 1;
+    multE = 1; is_signedE = 1; a = -2147483648; b = -2147483648;
     
-    start = 1; display; start = 0;
-    for (i = 1; i < 65; i = i + 1) display;
-
-    k = -2147483648 * -2147483648; //directly multiply to check the result
+    for (i = 0; i < 65; i = i + 1) display;
+    k = -2147483648 * -2147483648;
     
     //33329255 * 45825983 unsigned
-    a = 33329255; b = 45825983; is_signed = 0;
+    multE = 1; is_signedE = 0; a = 33329255; b = 45825983;
     
-    start = 1; display; start = 0;
-    for (i = 1; i < 65; i = i + 1) display;
-    k = 33329255 * 45825983; //directly multiply to check the result
+    for (i = 0; i < 65; i = i + 1) display;
+    k = 33329255 * 45825983;
     
     //0xF398_F1AB * 0xFFFF_FFFF unsigned
-    a = 32'hf398_f1ab; b = 32'hffff_ffff; is_signed = 0;
-    
-    start = 1; display; start = 0;
-    for (i = 1; i < 65; i = i + 1) display;
+    multE = 1; is_signedE = 0; a = 32'hf398_f1ab; b = 32'hffff_ffff;
 
-    k = 32'hf398_f1ab * 32'hffff_ffff; //directly multiply to check the result
+    for (i = 0; i < 65; i = i + 1) display;
+    k = 32'hf398_f1ab * 32'hffff_ffff;
     
-    start = 0; display; start = 0;
-    for (i = 1; i < 65; i = i + 1) display;
+    for (i = 0; i < 65; i = i + 1) display;
     $stop;
-
   end
   
-endmodule 
+endmodule
